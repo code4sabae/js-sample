4d4859df751b1521feb59613b10923058fb9c337
Taisuke Fukuno <fukuno@jig.jp>
2020-08-18 22:29:57 +0900

    update setup

--- setup-for-centos8.sh
+++ setup-for-centos8.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 curl -fsSL https://deno.land/x/install/install.sh | sh
-cat > .bash_profile << EOF
-export DENO_INSTALL="/root/.deno"
-export PATH="/root/.deno/bin:$PATH"
+cat >> .bash_profile << EOF
+export DENO_INSTALL=/root/.deno
+export PATH=/root/.deno/bin:$PATH
 EOF
-mkdir test
 source .bash_profile
+
+mkdir test
+cd test
 cat > main.js << EOF
 import { Server } from "https://code4sabae.github.io/js/Server.js";
 new Server(8881);
@@ -15,5 +17,8 @@ mkdir static
 cat > static/index.html << EOF
 Hi Deno!
 EOF
-cd ..
+
+firewall-cmd --permanent --add-port=8881/tcp
+firewall-cmd --reload
+
 nohup deno run -A main.js &
